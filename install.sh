f7df2d259ebad1d8ab6ddc8aa27d69137bb9ee54
manvd1 <ducman1998@gmail.com>
2021-06-11 15:44:57 +0700

    Add script to start demo

---
+++ install.sh
@@ -0,0 +1,26 @@
+conda create -n action-recognition python==3.7.9
+conda activate action-recognition
+
+pip install torch==1.6.0 torchvision==0.7.0
+pip install 'git+https://github.com/facebookresearch/fvcore'
+pip install simplejson
+conda install av -c conda-forge
+pip install -U iopath
+pip install psutil
+pip install opencv-python
+pip install torchvision
+pip install tensorboard
+pip install moviepy
+pip install pytorchvideo
+pip install sklearn
+
+# Detectron2
+pip install -U torch torchvision cython
+pip install -U 'git+https://github.com/facebookresearch/fvcore.git' 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
+git clone https://github.com/facebookresearch/detectron2 detectron2_repo
+pip install -e detectron2_repo
+
+# cuda vs cudnn
+conda install -c anaconda cudatoolkit=10.2
+conda install -c nvidia cudnn=8.0.4
+echo "Dependencies were installed successfully."
