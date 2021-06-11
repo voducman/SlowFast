58a246a3ea3886284f611cede5b1a4191215ebee
manvd1 <ducman1998@gmail.com>
2021-06-11 17:27:52 +0700

    Decreasing font size to 16

--- slowfast/visualization/video_visualizer.py
+++ slowfast/visualization/video_visualizer.py
@@ -466,7 +466,7 @@ class VideoVisualizer:
             )
         frame_visualizer = ImgVisualizer(frame, meta=None)
         font_size = min(
-            max(np.sqrt(frame.shape[0] * frame.shape[1]) // 35, 20), 20
+            max(np.sqrt(frame.shape[0] * frame.shape[1]) // 35, 16), 16
         )
         top_corner = not ground_truth
         if bboxes is not None:
