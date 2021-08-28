902a7f966867a08ba9a8ce532e71664008a0c3cc
voducman <ducman1998@gmail.com>
2021-08-28 15:10:57 +0700

    Update

--- scripts/start_demo_classify.sh
+++ scripts/start_demo_classify.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0
-conda activate action-recognition
 export PYTHONPATH=/u01/manvd1/action-recognition/SlowFast/slowfast:$PYTHONPATH
-test -f "logs/demo.txt" && rm logs/slowfast-demo.log | echo "Remove logs/slowfast-demo.log"
+conda activate action-recognition
+test -f "logs/slowfast-demo.log" && rm logs/slowfast-demo.log
 screen -L -Logfile logs/slowfast-demo.log -S slowfast-demo -m bash -c \
 "python tools/run_net.py \
 --cfg demo/Kinetics/SLOWFAST_8x8_R50.yaml \
