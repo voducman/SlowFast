effb7eca795770e357e84598763d5e74606b98c2
voducman <ducman1998@gmail.com>
2021-08-28 15:12:58 +0700

    Update

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,15 +2,15 @@ conda create -n action-recognition python==3.7.9 -y
 conda activate action-recognition
 
 # cuda vs cudnn
-conda install -c anaconda cudatoolkit=10.2
-conda install -c nvidia cudnn=8.0.4
+conda install -c anaconda cudatoolkit=10.2 -y
+conda install -c nvidia cudnn=8.0.4 -y
 
-pip install torch==1.6.0 torchvision==0.7.0
+pip install torch==1.6.0 torchvision==0.7.0 -y
 pip install 'git+https://github.com/facebookresearch/fvcore'
-pip install simplejson
+pip install simplejson -y
 conda install av -c conda-forge -y
-pip install -U iopath
-pip install psutil
+pip install -U iopath -y
+pip install psutil -y
 pip install opencv-python
 pip install torchvision
 pip install tensorboard
