39e1e2eef549be577fc1782f0e307d228c1a1d89
voducman <ducman1998@gmail.com>
2021-08-29 01:55:51 +0700

    Update train script

--- start_train.sh
+++ start_train.sh
@@ -1,4 +1,6 @@
-\conda activate action-recognition
-export PYTHONPATH=/u01/manvd1/action-recognition/SlowFast/slowfast:$PYTHONPATH
-rm slowfast-training.log
-screen -L -Logfile slowfast-training.log -S slowfast -m bash -c "CUDA_VISIBLE_DEVICES=4,5,6,7 python tools/run_net.py --cfg configs/Kinetics/custom/SLOWFAST_8x8_R50_FINE_TUNE.yaml"
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+conda activate action-recognition
+export PYTHONPATH=/u01/khienpv1/manvd1/action-recognition/SlowFast/slowfast:$PYTHONPATH
+test -f "logs/slowfast-train.log" && rm logs/slowfast-train.log
+screen -L -Logfile logs/slowfast-train.log -S slowfast -m bash -c \
+"python tools/run_net.py --cfg configs/Kinetics/custom/SLOWFAST_8x8_R50_FINE_TUNE.yaml"
