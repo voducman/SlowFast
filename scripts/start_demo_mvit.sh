25c842d9941e9bf76366f9cc1074d663345f5017
voducman <ducman1998@gmail.com>
2021-08-28 23:36:14 +0700

    Update

--- scripts/start_demo_mvit.sh
+++ scripts/start_demo_mvit.sh
@@ -6,10 +6,11 @@ screen -L -Logfile logs/mvit-demo.log -S mvit-demo -m bash -c \
 "python tools/run_net.py \
 --cfg demo/Kinetics/MVIT_B_16x4_CONV.yaml \
 TEST.CHECKPOINT_FILE_PATH checkpoints/K400_MVIT_B_16x4_CONV.pyth \
-TEST.CHECKPOINT_TYPE caffe2 \
+TEST.CHECKPOINT_TYPE pytorch \
 DEMO.VIS_MODE top-k \
 DEMO.ENABLE True \
 DEMO.LABEL_FILE_PATH /u01/khienpv1/manvd1/action-recognition/data/kinetics-400/class_id_mapping.json \
 DEMO.INPUT_VIDEO /u01/khienpv1/manvd1/action-recognition/demo \
+DEMO.THREAD_ENABLE True \
 OUTPUT_DIR ../demo/output
 "
