fef1110dccea2b9ecc01b015cf3ae0aa3fceb3da
voducman <ducman1998@gmail.com>
2021-08-30 00:30:56 +0700

    Reduce lr

--- start_tensorboard.sh
+++ start_tensorboard.sh
@@ -1,2 +1,3 @@
 #tensorboard --logdir output/training-log/runs-kinetics --port 10000 --bind_all
-tensorboard --logdir output/training-log-1e-4/runs-kinetics --port 10000 --bind_all
+#tensorboard --logdir output/training-log-1e-4/runs-kinetics --port 10000 --bind_all
+tensorboard --logdir output/training-log-1e-5/runs-kinetics --port 10000 --bind_all
