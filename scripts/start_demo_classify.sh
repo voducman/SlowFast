298267259abf62335002562f92666f618dfc080a
voducman <ducman1998@gmail.com>
2021-08-31 09:51:28 +0700

    Inference on new checkpoint

--- scripts/start_demo_classify.sh
+++ scripts/start_demo_classify.sh
@@ -4,11 +4,23 @@ conda activate action-recognition
 test -f "logs/slowfast-demo.log" && rm logs/slowfast-demo.log
 screen -L -Logfile logs/slowfast-demo.log -S slowfast-demo -m bash -c \
 "python tools/run_net.py \
---cfg demo/Kinetics/SLOWFAST_8x8_R50.yaml \
-TEST.CHECKPOINT_FILE_PATH checkpoints/SLOWFAST_8x8_R50.pkl \
+--cfg demo/Kinetics/SLOWFAST_8x8_R50_ft.yaml \
+TEST.CHECKPOINT_FILE_PATH checkpoints/checkpoint_epoch_00054_1e-4.pyth \
 TEST.CHECKPOINT_TYPE caffe2 \
 DEMO.ENABLE True \
 DEMO.LABEL_FILE_PATH /u01/khienpv1/manvd1/action-recognition/data/kinetics-400/class_id_mapping.json \
 DEMO.INPUT_VIDEO /u01/khienpv1/manvd1/action-recognition/demo \
-OUTPUT_DIR ../demo/output
+DEMO.VIS_MODE thresh \
+DEMO.COMMON_CLASS_THRES 0.7 \
+OUTPUT_DIR ../demo/output/thresh && \
+ \
+python tools/run_net.py \
+--cfg demo/Kinetics/SLOWFAST_8x8_R50_ft.yaml \
+TEST.CHECKPOINT_FILE_PATH checkpoints/checkpoint_epoch_00054_1e-4.pyth \
+TEST.CHECKPOINT_TYPE caffe2 \
+DEMO.ENABLE True \
+DEMO.LABEL_FILE_PATH /u01/khienpv1/manvd1/action-recognition/data/kinetics-400/class_id_mapping.json \
+DEMO.INPUT_VIDEO /u01/khienpv1/manvd1/action-recognition/demo \
+DEMO.VIS_MODE top-k \
+OUTPUT_DIR ../demo/output/top-k
 "
