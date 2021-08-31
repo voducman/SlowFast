a1cf550c9114b1eb6b6a4eb06b309cc7872ade13
voducman <ducman1998@gmail.com>
2021-08-31 10:04:29 +0700

    Fix

--- scripts/start_demo_classify.sh
+++ scripts/start_demo_classify.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=7
 export PYTHONPATH=/u01/manvd1/action-recognition/SlowFast/slowfast:$PYTHONPATH
 conda activate action-recognition
 test -f "logs/slowfast-demo.log" && rm logs/slowfast-demo.log
@@ -6,7 +6,7 @@ screen -L -Logfile logs/slowfast-demo.log -S slowfast-demo -m bash -c \
 "python tools/run_net.py \
 --cfg demo/Kinetics/SLOWFAST_8x8_R50_ft.yaml \
 TEST.CHECKPOINT_FILE_PATH checkpoints/checkpoint_epoch_00054_1e-4.pyth \
-TEST.CHECKPOINT_TYPE caffe2 \
+TEST.CHECKPOINT_TYPE pytorch \
 DEMO.ENABLE True \
 DEMO.LABEL_FILE_PATH /u01/khienpv1/manvd1/action-recognition/data/kinetics-400/class_id_mapping.json \
 DEMO.INPUT_VIDEO /u01/khienpv1/manvd1/action-recognition/demo \
@@ -17,7 +17,7 @@ OUTPUT_DIR ../demo/output/thresh && \
 python tools/run_net.py \
 --cfg demo/Kinetics/SLOWFAST_8x8_R50_ft.yaml \
 TEST.CHECKPOINT_FILE_PATH checkpoints/checkpoint_epoch_00054_1e-4.pyth \
-TEST.CHECKPOINT_TYPE caffe2 \
+TEST.CHECKPOINT_TYPE pytorch \
 DEMO.ENABLE True \
 DEMO.LABEL_FILE_PATH /u01/khienpv1/manvd1/action-recognition/data/kinetics-400/class_id_mapping.json \
 DEMO.INPUT_VIDEO /u01/khienpv1/manvd1/action-recognition/demo \
