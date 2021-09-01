acce4cf4d99064dd494acc04602725d7aaa0c003
voducman <ducman1998@gmail.com>
2021-09-01 11:30:11 +0700

    Fix

--- scripts/start_test_pretrained_10_klasses.sh
+++ scripts/start_test_pretrained_10_klasses.sh
@@ -4,7 +4,7 @@ export PYTHONPATH=/u01/manvd1/action-recognition/SlowFast/slowfast:$PYTHONPATH
 test -f "logs/slowfast-test.log" && rm logs/slowfast-test.log
 screen -L -Logfile logs/slowfast-test.log -S slowfast-test -m bash -c "python tools/run_net.py \
   --cfg configs/Kinetics/custom/SLOWFAST_8x8_R50.yaml \
-  DATA.PATH_TO_DATA_DIR /u01/manvd1/action-recognition/datasets/kinetics/val \
+  DATA.PATH_TO_DATA_DIR /u01/khienpv1/manvd1/action-recognition/data/kinetics-400/val \
   TEST.CHECKPOINT_FILE_PATH checkpoints/SLOWFAST_8x8_R50.pkl \
   TEST.CHECKPOINT_TYPE caffe2 \
   TRAIN.ENABLE False \
