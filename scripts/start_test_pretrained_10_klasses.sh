93d56d99702eb1d3fec5659f60b5b76b1b689f5e
voducman <ducman1998@gmail.com>
2021-09-01 11:24:53 +0700

    Test on val set using pre-train 400 classes

---
+++ scripts/start_test_pretrained_10_klasses.sh
@@ -0,0 +1,11 @@
+export CUDA_VISIBLE_DEVICES=2
+conda activate action-recognition
+export PYTHONPATH=/u01/manvd1/action-recognition/SlowFast/slowfast:$PYTHONPATH
+test -f "logs/slowfast-test.log" && rm logs/slowfast-test.log
+screen -L -Logfile logs/slowfast-test.log -S slowfast-test -m bash -c "python tools/run_net.py \
+  --cfg configs/Kinetics/custom/SLOWFAST_8x8_R50.yaml \
+  DATA.PATH_TO_DATA_DIR /u01/manvd1/action-recognition/datasets/kinetics/val \
+  TEST.CHECKPOINT_FILE_PATH checkpoints/SLOWFAST_8x8_R50.pkl \
+  TEST.CHECKPOINT_TYPE caffe2 \
+  TRAIN.ENABLE False \
+  OUTPUT_DIR ./output/test/pre-trained"
