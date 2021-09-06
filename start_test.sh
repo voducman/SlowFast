74767b3a16828e7cd8d7709bd8ab6733b6110537
voducman <ducman1998@gmail.com>
2021-09-06 16:59:50 +0700

    Add test script

---
+++ start_test.sh
@@ -0,0 +1,6 @@
+export CUDA_VISIBLE_DEVICES=4,5
+conda activate action-recognition
+export PYTHONPATH=/u01/khienpv1/manvd1/action-recognition/SlowFast/slowfast:$PYTHONPATH
+test -f "logs/slowfast-test.log" && rm logs/slowfast-test.log
+screen -L -Logfile logs/slowfast-test.log -S slowfast-test -m bash -c \
+"python tools/run_net.py --cfg configs/Kinetics/custom/SLOWFAST_8x8_R50_TEST.yaml"
