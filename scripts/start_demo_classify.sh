1ad222603f3f10fff3791921aab3079facc60079
voducman <ducman1998@gmail.com>
2021-09-18 10:25:25 +0700

    Update

--- scripts/start_demo_classify.sh
+++ scripts/start_demo_classify.sh
@@ -12,7 +12,7 @@ DEMO.LABEL_FILE_PATH /u01/khienpv1/manvd1/action-recognition/data/kinetics-400/c
 DEMO.INPUT_VIDEO  /u01/khienpv1/manvd1/action-recognition/slowfast-demo/demo/input \
 DEMO.VIS_MODE thres \
 DEMO.COMMON_CLASS_THRES 0.5 \
-OUTPUT_DIR output/demo/fine-tune/thresh && \
+OUTPUT_DIR output/demo/add-445/thresh && \
  \
 python tools/run_net.py \
 --cfg demo/Kinetics/SLOWFAST_8x8_R50_ft.yaml \
@@ -22,5 +22,5 @@ DEMO.ENABLE True \
 DEMO.LABEL_FILE_PATH /u01/khienpv1/manvd1/action-recognition/data/kinetics-400/class_id_mapping.json \
 DEMO.INPUT_VIDEO  /u01/khienpv1/manvd1/action-recognition/slowfast-demo/demo/input \
 DEMO.VIS_MODE top-k \
-OUTPUT_DIR output/demo/fine-tune/top-k
+OUTPUT_DIR output/demo/add-445/top-k
 "
