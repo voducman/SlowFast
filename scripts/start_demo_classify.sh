54ff1a977ef29838607f103ac2d4fecc979b2820
voducman <ducman1998@gmail.com>
2021-09-18 10:00:20 +0700

    Update

--- scripts/start_demo_classify.sh
+++ scripts/start_demo_classify.sh
@@ -5,22 +5,22 @@ test -f "logs/slowfast-demo.log" && rm logs/slowfast-demo.log
 screen -L -Logfile logs/slowfast-demo.log -S slowfast-demo -m bash -c \
 "python tools/run_net.py \
 --cfg demo/Kinetics/SLOWFAST_8x8_R50_ft.yaml \
-TEST.CHECKPOINT_FILE_PATH checkpoints/SLOWFAST_8x8_R50.pkl \
-TEST.CHECKPOINT_TYPE caffe2 \
+TEST.CHECKPOINT_FILE_PATH checkpoints/checkpoint_epoch_00054_1e-4.pyth \
+TEST.CHECKPOINT_TYPE pytorch \
 DEMO.ENABLE True \
-DEMO.LABEL_FILE_PATH /u01/server_100_backup/ComputerVision/ActionRecognition/datasets/kinetics/400/class_id_mapping.json \
+DEMO.LABEL_FILE_PATH /u01/khienpv1/manvd1/action-recognition/data/kinetics-400/class_id_mapping.json \
 DEMO.INPUT_VIDEO  /u01/khienpv1/manvd1/action-recognition/slowfast-demo/demo/input \
 DEMO.VIS_MODE thres \
 DEMO.COMMON_CLASS_THRES 0.5 \
-OUTPUT_DIR output/demo/pretrained/thresh && \
+OUTPUT_DIR output/demo/fine-tune/thresh && \
  \
 python tools/run_net.py \
 --cfg demo/Kinetics/SLOWFAST_8x8_R50_ft.yaml \
-TEST.CHECKPOINT_FILE_PATH checkpoints/SLOWFAST_8x8_R50.pkl \
-TEST.CHECKPOINT_TYPE caffe2 \
+TEST.CHECKPOINT_FILE_PATH checkpoints/checkpoint_epoch_00054_1e-4.pyth \
+TEST.CHECKPOINT_TYPE pytorch \
 DEMO.ENABLE True \
-DEMO.LABEL_FILE_PATH /u01/server_100_backup/ComputerVision/ActionRecognition/datasets/kinetics/400/class_id_mapping.json \
+DEMO.LABEL_FILE_PATH /u01/khienpv1/manvd1/action-recognition/data/kinetics-400/class_id_mapping.json \
 DEMO.INPUT_VIDEO  /u01/khienpv1/manvd1/action-recognition/slowfast-demo/demo/input \
 DEMO.VIS_MODE top-k \
-OUTPUT_DIR output/demo/pretrained/top-k
+OUTPUT_DIR output/demo/fine-tune/top-k
 "
