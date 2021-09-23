7a8a1e4303fd5d94ad8a6031f581b7771dfc7b64
manvd1 <ducman1998@gmail.com>
2021-09-23 11:28:37 +0700

    Inference with pretrain

--- scripts/start_demo_classify.sh
+++ scripts/start_demo_classify.sh
@@ -5,22 +5,10 @@ test -f "logs/slowfast-demo.log" && rm logs/slowfast-demo.log
 screen -L -Logfile logs/slowfast-demo.log -S slowfast-demo -m bash -c \
 "python tools/run_net.py \
 --cfg demo/Kinetics/SLOWFAST_8x8_R50_ft.yaml \
-TEST.CHECKPOINT_FILE_PATH checkpoints/checkpoint_epoch_00056_445.pyth \
-TEST.CHECKPOINT_TYPE pytorch \
+TEST.CHECKPOINT_FILE_PATH checkpoints/SLOWFAST_8x8_R50.pkl \
+TEST.CHECKPOINT_TYPE caffe2 \
 DEMO.ENABLE True \
-DEMO.LABEL_FILE_PATH /u01/khienpv1/manvd1/action-recognition/data/kinetics-400/class_id_mapping.json \
-DEMO.INPUT_VIDEO  /u01/khienpv1/manvd1/action-recognition/slowfast-demo/demo/input \
-DEMO.VIS_MODE thres \
-DEMO.COMMON_CLASS_THRES 0.5 \
-OUTPUT_DIR output/demo/add-445/thresh && \
- \
-python tools/run_net.py \
---cfg demo/Kinetics/SLOWFAST_8x8_R50_ft.yaml \
-TEST.CHECKPOINT_FILE_PATH checkpoints/checkpoint_epoch_00056_445.pyth \
-TEST.CHECKPOINT_TYPE pytorch \
-DEMO.ENABLE True \
-DEMO.LABEL_FILE_PATH /u01/khienpv1/manvd1/action-recognition/data/kinetics-400/class_id_mapping.json \
+DEMO.LABEL_FILE_PATH /nas_volume1/ComputerVision/DATA_CV/ActionRecognition/datasets/kinetics/400/class_id_mapping.json \
 DEMO.INPUT_VIDEO  /u01/khienpv1/manvd1/action-recognition/slowfast-demo/demo/input \
 DEMO.VIS_MODE top-k \
-OUTPUT_DIR output/demo/add-445/top-k
-"
+OUTPUT_DIR output/demo/pretrained/top-k"
