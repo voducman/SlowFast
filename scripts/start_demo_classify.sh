2e8fe4942ca1beb11dfda2d319c4e943ee348b37
manvd1 <ducman1998@gmail.com>
2021-09-25 10:46:05 +0700

    Demo with falling

--- scripts/start_demo_classify.sh
+++ scripts/start_demo_classify.sh
@@ -5,10 +5,10 @@ test -f "logs/slowfast-demo.log" && rm logs/slowfast-demo.log
 screen -L -Logfile logs/slowfast-demo.log -S slowfast-demo -m bash -c \
 "python tools/run_net.py \
 --cfg demo/Kinetics/SLOWFAST_8x8_R50_ft.yaml \
-TEST.CHECKPOINT_FILE_PATH checkpoints/checkpoint_epoch_00054_1e-4.pyth \
+TEST.CHECKPOINT_FILE_PATH checkpoints/checkpoint_epoch_00068_11_actions.pyth \
 TEST.CHECKPOINT_TYPE pytorch \
 DEMO.ENABLE True \
 DEMO.LABEL_FILE_PATH  /u01/khienpv1/manvd1/action-recognition/data/kinetics-400/class_id_mapping.json \
 DEMO.INPUT_VIDEO  /u01/khienpv1/manvd1/action-recognition/slowfast-demo/demo/input \
 DEMO.VIS_MODE top-k \
-OUTPUT_DIR output/demo/fine-tune/top-k"
+OUTPUT_DIR output/demo/fine-tune-11-actions/top-k"
